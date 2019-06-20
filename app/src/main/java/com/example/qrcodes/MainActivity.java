07f5218506a9008860b56058d8f5e848ebb950ca
Usuario02 <albertovazquez120.av@gmail.com>
2019-06-20 14:30:02 -0600

    Initial commit

---
+++ app/src/main/java/com/example/qrcodes/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.qrcodes;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraView camView;
+    private Button btnScan;
+    private AlertDialog alertDialog;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        camView.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        camView.stop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        camView = findViewById(R.id.cmView);
+        btnScan = findViewById(R.id.btnScan);
+
+        alertDialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Espere")
+                .setCancelable(false)
+                .build();
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camView.start();
+                camView.captureImage();
+            }
+        });
+
+        camView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap,camView.getWidth(), camView.getHeight(), false);
+                camView.stop();
+
+                runDetector(bitmap);
+
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+    }
+
+    private void runDetector(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionBarcodeDetectorOptions options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(
+                        FirebaseVisionBarcode.FORMAT_QR_CODE,
+                        FirebaseVisionBarcode.FORMAT_AZTEC
+                ).build();
+
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                processResult(firebaseVisionBarcodes);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes){
+        for(FirebaseVisionBarcode item : firebaseVisionBarcodes){
+            int value_type = item.getValueType();
+            switch (value_type){
+                case FirebaseVisionBarcode.TYPE_TEXT:{
+                    Builder builder = new Builder(this);
+                    builder.setMessage(item.getRawValue());
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                break;
+                case FirebaseVisionBarcode.TYPE_URL:{
+                        // Hacer Segundo Activity que tenga un web view y habra la pagina
+                    Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getRawValue()));
+                    startActivity(browser);
+                }
+                break;
+                case FirebaseVisionBarcode.TYPE_CONTACT_INFO:{
+                    String info= new StringBuilder("Name: ")
+                            .append(item.getContactInfo().getName().getFormattedName())
+                            .append("\n")
+                            .append("Adress: ")
+                            .append(item.getContactInfo().getAddresses().get(0).getAddressLines())
+                            .append("n")
+                            .append("Email: ")
+                            .append(item.getContactInfo().getEmails().get(0).getAddress())
+                            .toString();
+                    Builder builder = new Builder(this);
+                    builder.setMessage(info);
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                }
+                break;
+                default:
+                    break;
+            }
+
+
+        }
+        alertDialog.dismiss();
+    }
+
+}
